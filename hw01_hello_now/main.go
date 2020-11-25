16a29416b8ab96e2c1db581356652dd62139b65a
Ivan Artamonov <i.o.artamonov@gmail.com>
2020-11-26 00:45:33 +0200

    HW1

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-	// Place your code here
+	ntpTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		log.Fatalf("Could not get ntp time: %s", err)
+	}
+
+	println("current time: " + time.Now().Round(time.Second).String())
+	println("exact time: " + ntpTime.Round(time.Second).String())
 }
